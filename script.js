51ede6f66ebb25471aa67718cedceaeac6f7eb2b
xuziyue <xuziyue202109@163.com>
2023-11-30 23:57:40 +0800

    1

---
+++ script.js
@@ -0,0 +1,20 @@
+function DoubleNum(num){
+    if (num < 10){
+        return '0' + num;
+    }
+    else{
+        return '' + num;
+    }
+}
+
+function GetTime(){
+    Now = new Date();
+
+    var h = Now.getHours();
+    var m = Now.getMinutes();
+    var s = Now.getSeconds();
+
+    var Time = DoubleNum(h) + ':' + DoubleNum(m) + ':' + DoubleNum(s);
+
+    document.getElementById
+}
