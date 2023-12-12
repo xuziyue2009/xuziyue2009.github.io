4d68fec693a071fe2260cbc9382e542cbf685aac
xuziyue <xuziyue202109@163.com>
2023-12-12 20:30:54 +0800

    time?

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
-function DoubleNum(num){
-    if (num < 10){
-        return '0' + num;
-    }
-    else{
-        return '' + num;
-    }
+function updateClock(){
+    var now = new Date();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+
+    hour = (hour < 10 ? "0" : "") + hour;
+    minute = (minute < 10 ? "0" : "") + minute;
+    second = (second < 10 ? "0" : "") + second;
+
+    var time = hour + ":" + minute + ":" + second;
+    document.getElementById("time").innerHTML = time;
+
+    setTimeout(updateClock, 1000);
 }
 
-function GetTime(){
-    Now = new Date();
-
-    var h = Now.getHours();
-    var m = Now.getMinutes();
-    var s = Now.getSeconds();
-
-    var Time = DoubleNum(h) + ':' + DoubleNum(m) + ':' + DoubleNum(s);
-
-    document.getElementById("time").innerHTML = Time;
-    
-    setTimeout(GetTime, 1000);
-}
-
-GetTime();
+updateClock();
