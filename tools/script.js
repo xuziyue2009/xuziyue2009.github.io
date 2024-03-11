8aee92cb7cba5e5ddec4bf4d69c8842ab50030fc
xuziyue <xuziyue202109@163.com>
2024-03-11 17:08:19 +0800

    fenjieok

--- tools/script.js
+++ tools/script.js
@@ -9,3 +9,20 @@ function isprime(num){
     }
     return true;
 }
+
+function factorize(num) {
+    var factors = [];
+    
+    for (var i = 2; i <= Math.sqrt(num); i++) {
+        while (num % i === 0) {
+            factors.push(i);
+            num /= i;
+        }
+    }
+    
+    if (num > 1) {
+        factors.push(num);
+    }
+    
+    return factors;
+}
